502f39db5081f8fa222301a92fe56c5e029bb152
fsson <128136044+fsson@users.noreply.github.com>
2024-12-09 10:07:53 +0100

    Add creation of json to install and change own_window_type to override

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+touch mktdata.json
 cp conky-config conkyrc
 mv conkyrc ~/.conkyrc
 chmod +x conditional.sh
 chmod +x update_mktdata.sh
 
-echo "Config installed. Best of luck to your wojak!"
+echo "Config installed. Best of luck to your Wojak!"
